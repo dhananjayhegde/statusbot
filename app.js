00f037ca0a51d4935fcb77111122faebe4e64925
Dhananajy Hegde <dananjay123@gmail.com>
2018-06-18 09:49:36 +0530

    basic server set up

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.use((req, res, next) => {
+	res.status(200).json({
+		message : 'Hello World, (json)'
+	});
 });
 
-const port = process.env.PORT || 3000;
-app.listen(port, function () {
-  console.log('myapp listening on port ' + port);
-});
+
+module.exports = app;
